323f9c6723141cd1c7060df5926c3f80033518d1
paulndyeta <136905084+paulndyeta@users.noreply.github.com>
2024-02-04 18:42:43 +0300

    Add files via upload

---
+++ Controllers/FlightNumbersController.cs
@@ -0,0 +1,115 @@
+﻿namespace BalkanAir.Api.Controllers
+{
+    using System.Linq;
+    using System.Web.Http;
+    using System.Web.Http.Cors;
+
+    using AutoMapper;
+    using AutoMapper.QueryableExtensions;
+
+    using Common;
+    using Data.Helper;
+    using Data.Models;
+    using Models.FlightNumbers;
+    using Services.Data.Contracts;
+
+    /// <summary>
+    /// This controller must be accessible only from the Administrator.
+    /// </summary>
+
+    [EnableCors("*", "*", "*")]
+    [Authorize(Roles = UserRolesConstants.ADMINISTRATOR_ROLE)]
+    public class FlightNumbersController : ApiController
+    {
+        private readonly IFlightsServices flightsServices;
+        private readonly INumberGenerator numberGenerator;
+
+        public FlightNumbersController(IFlightsServices flightsServices, INumberGenerator numberGenerator)
+        {
+            this.flightsServices = flightsServices;
+            this.numberGenerator = numberGenerator;
+        }
+
+        [HttpPost]
+        public IHttpActionResult Create()
+        {
+            var flightNumberToAdd = new Flight() { Number = this.numberGenerator.GetUniqueFlightNumber() };
+            var addedFlightNumberId = this.flightsServices.AddFlight(flightNumberToAdd);
+
+            return this.Ok(addedFlightNumberId);
+        }
+
+        [HttpGet]
+        public IHttpActionResult All()
+        {
+            var flightNumbers = this.flightsServices.GetAll()
+                .OrderBy(f => f.Number)
+                .ProjectTo<FlightNumberResponseModel>()
+                .ToList();
+
+            return this.Ok(flightNumbers);
+        }
+
+        [HttpGet]
+        public IHttpActionResult Get(int id)
+        {
+            if (id <= 0)
+            {
+                return this.BadRequest(ErrorMessages.INVALID_ID);
+            }
+
+            var flightNumber = this.flightsServices.GetAll()
+                .ProjectTo<FlightNumberResponseModel>()
+                .FirstOrDefault(f => f.Id == id);
+
+            if (flightNumber == null)
+            {
+                return this.NotFound();
+            }
+
+            return this.Ok(flightNumber);
+        }
+
+        [HttpPut]
+        public IHttpActionResult Update(int id, UpdateFlightNumberRequestModel flightNumber)
+        {
+            if (id <= 0)
+            {
+                return this.BadRequest(ErrorMessages.INVALID_ID);
+            }
+
+            if (!this.ModelState.IsValid)
+            {
+                return this.BadRequest(this.ModelState);
+            }
+
+            var flightNumberToUpdate = Mapper.Map<Flight>(flightNumber);
+            var updatedFlightNumber = this.flightsServices.UpdateFlight(id, flightNumberToUpdate);
+
+            if (updatedFlightNumber == null)
+            {
+                return this.NotFound();
+            }
+
+            return this.Ok(updatedFlightNumber.Id);
+        }
+
+        [HttpDelete]
+        public IHttpActionResult Delete(int id)
+        {
+            if (id <= 0)
+            {
+                return this.BadRequest(ErrorMessages.INVALID_ID);
+            }
+
+            var deletedFlightNumber = this.flightsServices.DeleteFlight(id);
+
+            if (deletedFlightNumber == null)
+            {
+                return this.NotFound();
+            }
+
+            return this.Ok(deletedFlightNumber.Id);
+        }    
+    }
+}
---
+++ Controllers/UsersController.cs
@@ -0,0 +1,75 @@
+﻿namespace BalkanAir.Api.Controllers
+{
+    using System.Linq;
+    using System.Web.Http;
+    using System.Web.Http.Cors;
+
+    using AutoMapper.QueryableExtensions;
+
+    using Common;
+    using Data.Models;
+    using Models.Users;
+    using Services.Data.Contracts;
+
+    [EnableCors("*", "*", "*")]
+    public class UsersController : ApiController
+    {
+        private readonly IUsersServices usersServices;
+        
+        public UsersController(IUsersServices usersServices)
+        {
+            this.usersServices = usersServices;
+        }
+
+        [HttpGet]
+        public IHttpActionResult All()
+        {
+            var users = this.usersServices.GetAll()
+                .Where(u => !u.IsDeleted)
+                .OrderBy(u => u.UserSettings.FirstName)
+                .ThenBy(u => u.UserSettings.LastName)
+                .ProjectTo<UsersResponseModel>()
+                .ToList();
+
+            return this.Ok(users);
+        }
+
+        [HttpGet]
+        public IHttpActionResult GetUsersByGender(string gender)
+        {
+            if (gender.ToLower() != Gender.Male.ToString().ToLower() && 
+                gender.ToLower() != Gender.Female.ToString().ToLower())
+            {
+                return this.BadRequest(ErrorMessages.INVALID_GENDER);
+            }
+
+            var users = this.usersServices.GetAll()
+                .Where(u => !u.IsDeleted && u.UserSettings.Gender.ToString().ToLower() == gender.ToLower())
+                .ProjectTo<UsersResponseModel>()
+                .ToList();
+
+            return this.Ok(users);
+        }
+
+        [HttpGet]
+        public IHttpActionResult GetUsersByNationality(string nationality)
+        {
+            if (string.IsNullOrEmpty(nationality))
+            {
+                return this.BadRequest(ErrorMessages.NULL_OR_EMPTY_NATIONALITY);
+            }
+
+            var users = this.usersServices.GetAll()
+                .Where(u => !u.IsDeleted && u.UserSettings.Nationality.ToLower() == nationality.ToLower())
+                .ProjectTo<UsersResponseModel>()
+                .ToList();
+
+            if (users == null || users.Count == 0)
+            {
+                return this.NotFound();
+            }
+
+            return this.Ok(users);
+        }
+    }
+}
